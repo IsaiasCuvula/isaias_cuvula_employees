521c3c2b0692e3830417adc77c34b0ef00463ac6
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-03-28 21:22:58 +0300

    cleaned

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:isaias_cuvula_employees/presentation/states/projects_state_notifier.dart';
-import 'package:isaias_cuvula_employees/presentation/widgets/employee_data_source.dart';
+import 'package:isaias_cuvula_employees/presentation/widgets/widgets.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
 class HomePage extends ConsumerWidget {
@@ -12,6 +12,10 @@ class HomePage extends ConsumerWidget {
     final projectsStateNotifier = ref.watch(projectsStateNotifierProvider);
 
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           await ref.read(projectsStateNotifierProvider.notifier).pickCSVFile();
@@ -20,59 +24,51 @@ class HomePage extends ConsumerWidget {
       ),
       body: SafeArea(
         child: projectsStateNotifier.when(
-            loading: () => const Center(child: CircularProgressIndicator()),
-            error: (e, st) => Center(child: Text(e.toString())),
-            data: (employeesProjects) {
-              final employeeDataSource = EmployeeDataSource(
-                employeeData: employeesProjects,
-              );
-              return SfDataGrid(
-                source: employeeDataSource,
-                columnWidthMode: ColumnWidthMode.fill,
-                columns: [
-                  GridColumn(
-                    columnName: 'empID',
-                    label: Container(
-                      padding: const EdgeInsets.all(16.0),
-                      alignment: Alignment.center,
-                      child: const Text(
-                        'EmpID',
+          loading: () => const Center(child: CircularProgressIndicator()),
+          error: (e, st) => Center(child: Text(e.toString())),
+          data: (employeesProjects) {
+            final employeeDataSource = EmployeeDataGrid(
+              employeePairData: employeesProjects,
+            );
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    'Pair of employees who have worked together',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SfDataGrid(
+                  source: employeeDataSource,
+                  columnWidthMode: ColumnWidthMode.fill,
+                  columns: [
+                    GridColumn(
+                      columnName: 'Employee ID#1',
+                      label: const GridTitle(title: 'Employee ID#1'),
+                    ),
+                    GridColumn(
+                      columnName: 'Employee ID#2',
+                      label: const GridTitle(title: 'Employee ID#2'),
+                    ),
+                    GridColumn(
+                      columnName: 'ProjectID',
+                      label: const GridTitle(title: 'ProjectID'),
+                    ),
+                    GridColumn(
+                      columnName: 'Days Worked',
+                      label: const GridTitle(
+                        title: 'Days Worked',
                       ),
                     ),
-                  ),
-                  GridColumn(
-                    columnName: 'projectID',
-                    label: Container(
-                      padding: const EdgeInsets.all(16.0),
-                      alignment: Alignment.center,
-                      child: const Text(
-                        'ProjectID',
-                      ),
-                    ),
-                  ),
-                  GridColumn(
-                    columnName: 'dateFrom',
-                    label: Container(
-                      padding: const EdgeInsets.all(16.0),
-                      alignment: Alignment.center,
-                      child: const Text(
-                        'DateFrom',
-                      ),
-                    ),
-                  ),
-                  GridColumn(
-                    columnName: 'dateTo',
-                    label: Container(
-                      padding: const EdgeInsets.all(16.0),
-                      alignment: Alignment.center,
-                      child: const Text(
-                        'DateTo',
-                      ),
-                    ),
-                  ),
-                ],
-              );
-            }),
+                  ],
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
